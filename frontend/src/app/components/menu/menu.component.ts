d3fe615e7458cfb187d01ffd1748db7f35d2ec44
Alonso Aguero <aguerovilchez@gmail.com>
2023-02-16 01:40:31 -0500

    Cambio 16/02-v2

--- frontend/src/app/components/menu/menu.component.ts
+++ frontend/src/app/components/menu/menu.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CargarScriptsService } from 'src/app/cargarscript/cargar-scripts.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+
+  constructor(private _CargarScript:CargarScriptsService){
+    _CargarScript.CargarScripts(["menu"]);
+  }
+
+  ngOnInit(): void {
+  }
 
 }
