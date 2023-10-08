16048acdd065ac0f53fe07995faec25d410bf544
aaguero16 <aguerovilchez@gmail.com>
2023-10-07 22:50:19 -0500

    Update fonts
    Add fonts

---
+++ Proyecto Asistencia/client/src/app/styles/fonts.ts
@@ -0,0 +1,111 @@
+/************************** SOURCE CODE PRO **************************/
+
+/*<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+ 
+ 
+ @import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200;300;400;500;600;700&display=swap');*/
+
+
+    /********************************* TITULOS Y SUBTITULOS ********************************/
+
+    export class ad_h1 {
+      static primary = {
+        fontFamily: "'Source Code Pro', monospace",
+        fontSize: '48px',
+        fontWeight: 'bold',
+      };
+    }
+
+
+    export class ad_h2 {
+      static primary = {
+        fontFamily: "'Source Code Pro', monospace",
+        fontSize: '36px',
+        fontWeight: 'bold',
+      };
+    }
+
+    export class ad_h3 {
+      static primary = {
+        fontFamily: "'Source Code Pro', monospace",
+        fontSize: '24px',
+        fontWeight: 'bold',
+      };
+    }
+
+
+    export class ad_h4 {
+      static primary = {
+        fontFamily: "'Source Code Pro', monospace",
+        fontSize: '18px',
+        fontWeight: 'bold',
+      };
+    }
+
+
+    export class ad_h5 {
+      static primary = {
+        fontFamily: "'Source Code Pro', monospace",
+        fontSize: '16px',
+        fontWeight: 'bold',
+      };
+    }
+
+    export class ad_h6 {
+      static primary = {
+        fontFamily: "'Source Code Pro', monospace",
+        fontSize: '14px',
+        fontWeight: 'bold',
+      };
+    }
+
+
+
+    /********************************* OTROS TEXTOS ********************************/
+
+    export class ad_span {
+      static primary = {
+        fontFamily: "'Source Code Pro', monospace",
+        fontSize: '14px',
+        fontWeight: '300',
+      };
+    }
+
+    export class ad_p {
+      static primary = {
+        fontFamily: "'Source Code Pro', monospace",
+        fontSize: '14px',
+        fontWeight: '500',
+      };
+    }
+
+    export class ad_a {
+      static primary = {
+        fontFamily: "'Source Code Pro', monospace",
+        fontSize: '14px',
+        fontWeight: '600',
+      };
+    }
+
+
+
+
+    /********************************* TEXTOS PARA TABLA ********************************/
+
+    export class ad_span_tb {
+        static primary = {
+          fontFamily: "'Source Code Pro', monospace",
+          fontSize: '12px',
+          fontWeight: '300',
+        };
+    }
+
+    export class ad_head_tb {
+        static primary = {
+          fontFamily: "'Source Code Pro', monospace",
+          fontSize: '14px',
+          fontWeight: '600',
+        };
+    }
